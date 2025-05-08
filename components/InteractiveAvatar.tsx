14b45c9b9da5b4a6d8ad77c34b6ff4535efcb1f2
Dmitry Utlik <102942130+utlik-pro@users.noreply.github.com>
2025-05-08 11:39:44 +0300

    Update InteractiveAvatar.tsx

--- components/InteractiveAvatar.tsx
+++ components/InteractiveAvatar.tsx
@@ -1,39 +1,14 @@
 import {
   AvatarQuality,
-  StreamingEvents,
-  VoiceChatTransport,
-  VoiceEmotion,
   StartAvatarRequest,
-  STTProvider,
-  ElevenLabsModel,
 } from "@heygen/streaming-avatar";
-import { useEffect, useRef, useState } from "react";
-import { useMemoizedFn, useUnmount } from "ahooks";
 
-import { Button } from "./Button";
-// import { AvatarConfig } from "./AvatarConfig"; // Удалено по задаче
 import { AvatarVideo } from "./AvatarSession/AvatarVideo";
-import { useStreamingAvatarSession } from "./logic/useStreamingAvatarSession";
 import { AvatarControls } from "./AvatarSession/AvatarControls";
-import { useVoiceChat } from "./logic/useVoiceChat";
-import {
-  StreamingAvatarProvider,
-  StreamingAvatarSessionState,
-} from "./logic";
-import { LoadingIcon } from "./Icons";
 import { MessageHistory } from "./AvatarSession/MessageHistory";
+import { StreamingAvatarProvider } from "./logic";
 
 import { AVATARS } from "@/app/lib/constants";
-
-const DEFAULT_CONFIG: StartAvatarRequest = {
-  quality: AvatarQuality.Low,
-  avatarName: AVATARS[0].avatar_id,
-  knowledgeId: undefined,
-  voice: {
-    provider: "elevenlabs",
-    voice_id: "default",
-  },
-};
 
 export default function InteractiveAvatar() {
   return (
